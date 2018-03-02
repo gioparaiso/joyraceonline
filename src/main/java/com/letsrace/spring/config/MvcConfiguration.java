f67df4aedec75c45338e15100b3d53dbbea2b601
gioparaiso <gio_paraiso@yahoo.com>
2018-03-02 15:18:58 +0800

    Update MvcConfiguration.java

--- src/main/java/com/letsrace/spring/config/MvcConfiguration.java
+++ src/main/java/com/letsrace/spring/config/MvcConfiguration.java
@@ -82,7 +82,7 @@ public class MvcConfiguration extends WebMvcConfigurerAdapter {
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
 
 		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-		dataSource.setUrl("jdbc:mysql://10.131.109.196:3306/joyracedb");
+		dataSource.setUrl("jdbc:mysql://127.0.0.1:3306/joyracedb");
 		dataSource.setUsername("user7CB");
 		dataSource.setPassword("LpkN2LufloqdyJPy");
 
